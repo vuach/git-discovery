da3a3de8a65d5d84e4017ad17dd57dbfa138c1ba
Резаев Малинка <vuacheslavoo9@gmail.com>
2026-04-06 12:35:42 +0300

    new task upd plus rndwow1

--- malinka/8-bit-added.py
+++ malinka/8-bit-added.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from random import randint
 GPIO.setmode(GPIO.BCM)
-leds = [16,12,25,17,27,23,22,24]
+leds = [16,20,21,25,26,17,27,22]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 
@@ -16,10 +16,27 @@ def voltage_to_number(voltage):
 def dec2bin(val):
     return[int(el) for el in bin(val)[2:].zfill(8)]
 
-
 while True:
     voltage = randint(0,255)
     n = dec2bin(voltage)
     GPIO.output(leds, n)
     print(voltage)
     time.sleep(0.3)
+    GPIO.output(leds, 0)
+    time.sleep(0.15)
+
+
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose=False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    def set_number(self, number):
+        return[int(el) for el in bin(number)[2:].zfill(8)]
+    def set_voltage(self, voltage): pass
